23a5c4690398e0242621717a846e0c33e3959550
Admin <harunrashid9159@gmail.com>
2025-02-04 08:18:12 +0530

    Code Completed till 22:44

--- app/src/main/java/np/com/bimalkafle/geminiaichatbot/HomeViewModel.kt
+++ app/src/main/java/np/com/bimalkafle/geminiaichatbot/HomeViewModel.kt
@@ -2,25 +2,68 @@ package np.com.bimalkafle.geminiaichatbot
 
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.BuildConfig
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
 import com.google.ai.client.generativeai.type.generationConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class HomeViewModel: ViewModel() {
+
+    private val _uiState: MutableStateFlow<HomeUiState> = MutableStateFlow(HomeUiState.Initial)
+    private val uiState=  _uiState.asStateFlow()
+
+    private var generativeModel: GenerativeModel
 
 init{
     val config = generationConfig {
         temperature = 0.70f
     }
 
-    val generativeMode = GenerativeModel(
+    generativeModel = GenerativeModel(
         modelName = "gemini-pro-vision",
         apiKey = BuildConfig.apiKey,
         generationConfig = config
     )
 }
     fun questioning(userInput: String, selectedImage: List<Bitmap>) {
+        _uiState.value = HomeUiState.Loading
+        val prompt = "Take a Look at images, and then answer the following question: $userInput"
 
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                val content = content {
+                    for (bitmap in selectedImage){
+                        image(bitmap)
+                    }
+                    text(prompt)
+                }
+
+                var output = ""
+                generativeModel.generateContentStream(content).collect{
+                    output += it.text
+                    _uiState.value = HomeUiState.Success(output)
+                }
+
+            }catch (e: Exception){
+                _uiState.value = HomeUiState.Error(e.localizedMessage ?:"Error in Generative content")
+            }
+        }
     }
 }
 
+sealed interface HomeUiState{
+    object Initial: HomeUiState
+    object Loading: HomeUiState
+    data class Success(
+        val outputText: String
+    ): HomeUiState
+    data class Error (val error: String) : HomeUiState
+
+
+
+}
